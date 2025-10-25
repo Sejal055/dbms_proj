42c8fffbc23e23be65754595f136b20c87b529b7
Sonal453 <karekarsonal16@gmail.com>
2025-10-25 10:34:55 +0530

    made some fixes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,10 +1,59 @@
+// plugins {
+//     id("com.android.application")
+//     id("org.jetbrains.kotlin.android")
+//     // START: FlutterFire Configuration
+//     id("com.google.gms.google-services")
+//     // END: FlutterFire Configuration
+//     //id("kotlin-android")
+    
+
+//     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+//     id("dev.flutter.flutter-gradle-plugin")
+// }
+
+// android {
+//     namespace = "com.example.dbms_pro"
+//     compileSdk = flutter.compileSdkVersion
+//     ndkVersion = flutter.ndkVersion
+
+//     compileOptions {
+//         sourceCompatibility = JavaVersion.VERSION_11
+//         targetCompatibility = JavaVersion.VERSION_11
+//     }
+
+//     kotlinOptions {
+//         jvmTarget = JavaVersion.VERSION_11.toString()
+//     }
+
+//     defaultConfig {
+//         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+//         applicationId = "com.example.dbms_pro"
+//         // You can update the following values to match your application needs.
+//         // For more information, see: https://flutter.dev/to/review-gradle-config.
+//         minSdk = flutter.minSdkVersion
+//         targetSdk = flutter.targetSdkVersion
+//         versionCode = flutter.versionCode
+//         versionName = flutter.versionName
+//     }
+
+//     buildTypes {
+//         release {
+//             // TODO: Add your own signing config for the release build.
+//             // Signing with the debug keys for now, so `flutter run --release` works.
+//             signingConfig = signingConfigs.getByName("debug")
+//         }
+//     }
+// }
+
+// flutter {
+//     source = "../.."
+// }
 plugins {
     id("com.android.application")
-    // START: FlutterFire Configuration
+    id("org.jetbrains.kotlin.android")
+    // FlutterFire
     id("com.google.gms.google-services")
-    // END: FlutterFire Configuration
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    // Must be last
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -14,19 +63,17 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.dbms_pro"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -35,8 +82,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -44,4 +89,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.3")
 }
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,64 @@
+// allprojects {
+//     repositories {
+//         google()
+//         mavenCentral()
+//     }
+// }
+
+// val newBuildDir: Directory =
+//     rootProject.layout.buildDirectory
+//         .dir("../../build")
+//         .get()
+// rootProject.layout.buildDirectory.value(newBuildDir)
+
+// subprojects {
+//     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+//     project.layout.buildDirectory.value(newSubprojectBuildDir)
+// }
+// subprojects {
+//     project.evaluationDependsOn(":app")
+// }
+
+// tasks.register<Delete>("clean") {
+//     delete(rootProject.layout.buildDirectory)
+// }
+
+// plugins {
+//     // This ensures compatibility with Gradle 8.12 and Java 17
+//     id("com.android.application") version "8.6.0" apply false
+//     id("com.android.library") version "8.6.0" apply false
+//     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+// }
+
+// allprojects {
+//     repositories {
+//         google()
+//         mavenCentral()
+//     }
+// }
+
+// val newBuildDir = rootProject.layout.buildDirectory
+//     .dir("../../build")
+//     .get()
+// rootProject.layout.buildDirectory.value(newBuildDir)
+
+// subprojects {
+//     val newSubprojectBuildDir = newBuildDir.dir(project.name)
+//     project.layout.buildDirectory.value(newSubprojectBuildDir)
+//     project.evaluationDependsOn(":app")
+// }
+
+// tasks.register<Delete>("clean") {
+//     delete(rootProject.layout.buildDirectory)
+// }
+
+plugins {
+    // Remove version numbers — settings.gradle.kts already defines them
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    id("org.jetbrains.kotlin.android") apply false
+}
+
 allprojects {
     repositories {
         google()
@@ -5,17 +66,14 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
+val newBuildDir = rootProject.layout.buildDirectory
+    .dir("../../build")
+    .get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    val newSubprojectBuildDir = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
     project.evaluationDependsOn(":app")
 }
 
